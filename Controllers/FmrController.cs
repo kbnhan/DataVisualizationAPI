488a1a7f0526192803b02d2a12da355511789573
Kevin <kbn.nhan@gmail.com>
2022-02-15 10:29:03 -0600

    Added FmrController and moved WeatherForecast under models.

---
+++ Controllers/FmrController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataVisualizationAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FmrController : ControllerBase
+{
+
+}
