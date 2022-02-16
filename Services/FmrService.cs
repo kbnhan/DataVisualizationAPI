2f9aed1174aea396510c669cf1ea534fd0206374
Kevin <kbn.nhan@gmail.com>
2022-02-16 14:21:30 -0600

    Added DbContext and GET methods to FmrService.

--- Services/FmrService.cs
+++ Services/FmrService.cs
@@ -6,6 +6,25 @@ namespace DataVisualizationAPI.Services
 {
     public class FmrService
     {
-        
+        private readonly DataViz_ProjectContext _context;
+
+        public FmrService(DataViz_ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public FairMarketRent? GetById(int id)
+        {
+            return _context.FairMarketRents
+                .AsNoTracking()
+                .Single(f => f.Id == id);
+        }
+
+        public IQueryable<FairMarketRent> GetAreanames(string state)
+        {
+            return _context.FairMarketRents
+                .AsNoTracking()
+                .Where(f => f.State == state);
+        }
     }
 }
