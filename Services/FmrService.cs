a9f98d6decbb77f2fc57ad2accacadedba590679
Kevin <kbn.nhan@gmail.com>
2022-02-17 14:10:37 -0600

    Added queries for getting FMRs by year.

--- Services/FmrService.cs
+++ Services/FmrService.cs
@@ -26,5 +26,28 @@ namespace DataVisualizationAPI.Services
                 .AsNoTracking()
                 .Where(f => f.State == state);
         }
+
+        public IQueryable<FairMarketRent> GetFairMarketRents(string areaname, short year)
+        {
+            return _context.FairMarketRents
+                .AsNoTracking()
+                .Where
+                (
+                    f => f.Areaname == areaname 
+                    && f.Year == year
+                );
+        }
+
+        public IQueryable<FairMarketRent> GetFairMarketRents(string areaname, short startYear, short endYear)
+        {
+            return _context.FairMarketRents
+                .AsNoTracking()
+                .Where
+                (
+                    f => f.Areaname == areaname
+                    && f.Year >= startYear
+                    && f.Year <= endYear
+                );
+        }
     }
 }
